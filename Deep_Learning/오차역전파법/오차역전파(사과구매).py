babb1706bf30e1ec1abbb290377eaac0ac35ccd2
SunSet0418 <cds07016@naver.com>
2017-09-10 18:55:08 +0900

    오차역전파법

--- Deep_Learning/오차역전파법/오차역전파(사과구매).py
+++ Deep_Learning/오차역전파법/오차역전파(사과구매).py
@@ -1,4 +1,24 @@
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+class AddLayer:
+
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        return x+y
+
+    def backward(self, dout):
+        dx = dout*1
+        dy = dout*1
+        return dx, dy
+
 class MulLayer:
+
     def __init__(self):
         self.x = None
         self.t = None
@@ -18,3 +38,13 @@ class MulLayer:
         return dx, dy
 
 
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+mul_tax_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward()
+price = mul_tax_layer.forward(apple_price, tax)
+
+print(price)

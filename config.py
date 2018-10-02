86f41577de31fc5554be4c6c5cca478f3ea92c9b
SunSet0418 <coffee@soylatte.kr>
2018-10-02 16:06:00 +0900

    rename files

--- config.py
+++
@@ -1,6 +0,0 @@
-c=get_config()
-c.NotebookApp.password=u'sha1:88ec1130d03f:bb3f54ad4490b2b0a69d78a344d820f827007d55'
-c.NotebookApp.ip='0.0.0.0'
-c.NotebookApp.port=5000
-#c.NotebookApp.certfile = u'/home/sunset/ssl/soylatte.kr_20170221I7NV.crt.pem'
-#c.NotebookApp.keyfile = u'/home/sunset/ssl/soylatte.kr_20170221I7NV.key.pem'
